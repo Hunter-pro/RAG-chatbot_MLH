dfbb8b99833a131ab9598fa8f7be8f200db965e9
Hunter-pro <kushalcherukula@gmail.com>
2025-02-13 15:25:33 +0530

    added steamlit pages

---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("This is my first Chatbot app")
+st.divider()
+st.write("interact with the chatbot using the side bar")
+st.image("https://creator.nightcafe.studio/jobs/sj60e9ymt8wiEiN5wT0u/sj60e9ymt8wiEiN5wT0u--1--zawav.jpg")
+st.subheader("built by @kushalcherukula👍")
+
